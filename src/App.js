ccefb7426f1098c08d9a4b0e686c891b7078f5f7
serges <stchoutatgoum@gmail.ca>
2020-02-11 03:13:00 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,84 +1,108 @@
-import React,{Component} from 'react';
-import './App.css';
-import { Link,Route, Switch, Redirect } from 'react-router-dom';
-import Sales from './Components/Sales';
-import Sale from './Components/Sale';
+import React, { Component } from 'react'
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavDropdown,
+  FormGroup,
+  FormControl,
+  MenuItem,
+  Grid,
+  Row,
+  Col
+} from 'react-bootstrap'
+import { Link, Switch, Redirect, Route, withRouter } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
 
-import NotFound from './Components/NotFound';
-import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem, FormGroup, FormControl, Grid, Row, Col } from 'react-bootstrap';
+import Sales from './Components/Sales'
+import Sale from './Components/Sale'
+import NotFound from './Components/NotFound'
+import './App.css'
 
 class App extends Component {
-  state={recentlyViewed:[searchId]};
+  state = { recentlyViewed: [], searchId: '' }
 
-
-   viewedSale=(id)=>{
-    if(state.recentlyViewed.indexOf(id) === -1) {
-      this.setState(({recentlyViewed})=>({
+  viewedSale = (id) => {
+    const notInArray = this.state.recentlyViewed.indexOf(id) === -1
+    if (notInArray)
+      this.setState(({ recentlyViewed }) => ({
         recentlyViewed: [...recentlyViewed, id]
       }))
-    }
-    updateSearchId=(e)=>this.setState({[e.target.name]:e.target.value})
-    render(){
-      if(this.props.location.pathname==='/')
-      return <Redirect exact push to='/Sales'/>
-   
-  return (
-    <div>
-      <Navbar inverse collapseOnSelect staticTop>
-  <Navbar.Header>
-    <LinkContainer to="/">
-      <Navbar.Brand>
-        WEB422 - Sales
-      </Navbar.Brand>
-    </LinkContainer>
-    <Navbar.Toggle />
-  </Navbar.Header>
-  <Navbar.Collapse>
-    <Nav>
-    <LinkContainer to="/Sales">
-      <NavItem>All Sales</NavItem>
-    </LinkContainer>
-      <NavDropdown title="Previously Viewed" id="basic-nav-dropdown">
-      {this.state.recentlyViewed.length > 0 ? 
-        this.state.recentlyViewed.map((id, index)=>( 
-          <LinkContainer to={`/Sale/${id}`} key={index}>
-            <MenuItem >Sale: {id}</MenuItem>
-          </LinkContainer> )) : 
-        <MenuItem>...</MenuItem>}
-      </NavDropdown>
-    </Nav>
-    <Navbar.Form pullRight>
-      <FormGroup>
-        <FormControl type="text" onChange={this.updateSearchId} placeholder="Sale ID" />
-      </FormGroup>{' '}
-      <Link className="btn btn-default" to={"/Sale/" + this.state.searchId}>Search</Link>
-    </Navbar.Form>
-  </Navbar.Collapse>
-</Navbar>
+  }
 
+  updateSearchId = (e) => this.setState({ [e.target.name]: e.target.value })
 
-      <br /><br /><br />
+  render() {
+    if (this.props.location.pathname === '/')
+      return <Redirect exact push to='/Sales' />
 
-      <Grid>
-        <Row>
-          <Col md={12}>
-            <Switch>
-              <Route exact path='/'/>
-              <Route exact path='/Sales' Component={Sales}/>
-              <Route path='/Sale/:id' render={(props) => (
-                <Sale id={props.match.params.id} viewedSale={this.viewedSale}/>
-              )} />
-              
-              <Route render={() => (
-                <NotFound />
-              )} />
-            </Switch>
-          </Col>
-        </Row>
-      </Grid>
-    </div>
-  );
-}}}
+    return (
+      <div>
+        <Navbar inverse collapseOnSelect staticTop>
+          <Navbar.Header>
+            <LinkContainer to='/'>
+              <Navbar.Brand>WEB422 -Sales</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <LinkContainer to='/Sales'>
+                <NavItem>All Sales</NavItem>
+              </LinkContainer>
+              <NavDropdown title='Previously Viewed' id='basic-nav-dropdown'>
+                {this.state.recentlyViewed.length > 0 ? (
+                  this.state.recentlyViewed.map((id, index) => (
+                    <LinkContainer to={`/Sale/${id}`} key={index}>
+                      <MenuItem>Sale: {id}</MenuItem>
+                    </LinkContainer>
+                  ))
+                ) : (
+                  <MenuItem>...</MenuItem>
+                )}
+              </NavDropdown>
+            </Nav>
+            <Navbar.Form pullRight>
+              <FormGroup>
+                <FormControl
+                  type='text'
+                  onChange={this.updateSearchId}
+                  placeholder='Sale ID'
+                />
+              </FormGroup>{' '}
+              <Link
+                className='btn btn-default'
+                to={'/Sale/' + this.state.searchId}
+              >
+                Search
+              </Link>
+            </Navbar.Form>
+          </Navbar.Collapse>
+        </Navbar>
 
-export default withRouter(App);
+        <Grid>
+          <Row>
+            <Col md={12}>
+              <Switch>
+                <Route exact path='/' />
+                <Route path='/Sales' component={Sales} />
+                <Route
+                  path='/Sale/:id'
+                  render={(props) => (
+                    <Sale
+                      id={props.match.params.id}
+                      viewedSale={this.viewedSale}
+                    />
+                  )}
+                />
+                <Route render={() => <NotFound />} />
+              </Switch>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default withRouter(App)
