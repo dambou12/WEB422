6ad1d2959d8c39f55722175c1e2ccae34f6f4f4c
serges <stchoutatgoum@gmail.ca>
2020-02-11 02:22:52 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,64 +1,84 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import Products from './Products';
-import Product from './Product';
+import { Link,Route, Switch, Redirect } from 'react-router-dom';
+import Sales from './Components/Sales';
+import Sale from './Components/Sale';
 import About from './About';
-import NotFound from './NotFound';
+import NotFound from './Components/NotFound';
 import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, FormGroup, FormControl, Grid, Row, Col } from 'react-bootstrap';
 
-function App() {
+class App extends Component {
+  state={recentlyViewed:[searchId]};
+
+
+   viewedSale=(id)=>{
+    if(state.recentlyViewed.indexOf(id) === -1) {
+      this.setState(({recentlyViewed})=>({
+        recentlyViewed: [...recentlyViewed, id]
+      }))
+    }
+    updateSearchId=(e)=>this.setState({[e.target.name]:e.target.value})
+    render(){
+      if(this.props.location.pathname==='/')
+      return <Redirect exact push to='/Sales'/>
+   
   return (
     <div>
-      <Navbar inverse collapseOnSelect fixedTop>
-        <Navbar.Header>
-          <LinkContainer to="/">
-            <Navbar.Brand>
-              WEB422 - React Routing
-            </Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <LinkContainer to="/Products">
-              <NavItem eventKey={1}>Products</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/About">
-              <NavItem eventKey={2}>About</NavItem>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Navbar inverse collapseOnSelect staticTop>
+  <Navbar.Header>
+    <LinkContainer to="/">
+      <Navbar.Brand>
+        WEB422 - Sales
+      </Navbar.Brand>
+    </LinkContainer>
+    <Navbar.Toggle />
+  </Navbar.Header>
+  <Navbar.Collapse>
+    <Nav>
+    <LinkContainer to="/Sales">
+      <NavItem>All Sales</NavItem>
+    </LinkContainer>
+      <NavDropdown title="Previously Viewed" id="basic-nav-dropdown">
+      {this.state.recentlyViewed.length > 0 ? 
+        this.state.recentlyViewed.map((id, index)=>( 
+          <LinkContainer to={`/Sale/${id}`} key={index}>
+            <MenuItem >Sale: {id}</MenuItem>
+          </LinkContainer> )) : 
+        <MenuItem>...</MenuItem>}
+      </NavDropdown>
+    </Nav>
+    <Navbar.Form pullRight>
+      <FormGroup>
+        <FormControl type="text" onChange={this.updateSearchId} placeholder="Sale ID" />
+      </FormGroup>{' '}
+      <Link className="btn btn-default" to={"/Sale/" + this.state.searchId}>Search</Link>
+    </Navbar.Form>
+  </Navbar.Collapse>
+</Navbar>
+
 
       <br /><br /><br />
 
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
+      <Grid>
+        <Row>
+          <Col md={12}>
             <Switch>
-              <Route exact path='/' render={() => (
-                <Redirect push to={"/Products"} />
+              <Route exact path='/'/>
+              <Route exact path='/Sales' Component={Sales}/>
+              <Route path='/Sale/:id' render={(props) => (
+                <Sale id={props.match.params.id} viewedSale={this.viewedSale}/>
               )} />
-              <Route exact path='/Products' render={() => (
-                <Products />
-              )} />
-              <Route path='/Product/:id' render={(props) => (
-                <Product id={props.match.params.id} />
-              )} />
-              <Route exact path='/About' render={() => (
-                <About />
-              )} />
+              
               <Route render={() => (
                 <NotFound />
               )} />
             </Switch>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Grid>
     </div>
   );
-}
+}}}
 
-export default App;
+export default withRouter(App);
